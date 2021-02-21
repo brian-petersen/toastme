fe089b9af24987bb1835e6db73053cdf3ae56820
Brian Petersen <skiwalk3r@gmail.com>
2021-02-20 21:33:41 -0700

    Adds redirecting from auth to match page

--- lib/toastme_web/authentication.ex
+++ lib/toastme_web/authentication.ex
@@ -4,4 +4,8 @@ defmodule ToastMeWeb.Authentication do
   def signed_in?(conn) do
     not is_nil(Conn.get_session(conn, :current_user_id))
   end
+
+  def current_user_id(conn) do
+    Conn.get_session(conn, :current_user_id)
+  end
 end
--- lib/toastme_web/controllers/pages_controller.ex
+++ lib/toastme_web/controllers/pages_controller.ex
@@ -1,13 +1,30 @@
 defmodule ToastMeWeb.PagesController do
   use ToastMeWeb, :controller
 
+  alias ToastMe.Profiles
   alias ToastMeWeb.Authentication
 
   def home(conn, _params) do
-    if Authentication.signed_in?(conn) do
-      redirect(conn, to: Routes.setup_path(conn, :index))
-    else
-      render(conn, "home.html")
+    signed_in? = Authentication.signed_in?(conn)
+
+    cond do
+      signed_in? and has_setup_profile?(conn) ->
+        redirect(conn, to: Routes.match_path(conn, :index))
+
+      signed_in? ->
+        redirect(conn, to: Routes.setup_path(conn, :index))
+
+      true ->
+        render(conn, "home.html")
     end
+  end
+
+  defp has_setup_profile?(conn) do
+    profile =
+      conn
+      |> Authentication.current_user_id()
+      |> Profiles.get_for_user()
+
+    profile != nil
   end
 end
