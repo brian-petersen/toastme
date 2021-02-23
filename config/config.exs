f4d67c255ea15e2c641326555c32143a4403db4e
Brian Petersen <skiwalk3r@gmail.com>
2021-02-22 21:43:51 -0700

    Updates config files for production

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,4 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-
-# General application configuration
-use Mix.Config
+import Config
 
 config :toastme,
   namespace: ToastMe,
@@ -40,4 +33,4 @@ config :ueberauth, Ueberauth,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+import_config "#{config_env()}.exs"
--- config/runtime.exs
+++ config/runtime.exs
@@ -14,3 +14,31 @@ end
 config :ueberauth, Ueberauth.Strategy.Facebook.OAuth,
   client_id: facebook_client_id,
   client_secret: facebook_client_secret
+
+if config_env() == :prod do
+  database_url =
+    System.get_env("DATABASE_URL") ||
+      raise """
+      environment variable DATABASE_URL is missing.
+      For example: ecto://USER:PASS@HOST/DATABASE
+      """
+
+  config :toastme, ToastMe.Repo,
+    url: database_url,
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
+  secret_key_base =
+    System.get_env("SECRET_KEY_BASE") ||
+      raise """
+      environment variable SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
+
+  config :toastme, ToastMeWeb.Endpoint,
+    http: [
+      port: String.to_integer(System.get_env("PORT") || "4000"),
+      transport_options: [socket_opts: [:inet6]]
+    ],
+    secret_key_base: secret_key_base,
+    server: true
+end
